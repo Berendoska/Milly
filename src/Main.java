2cc7fb48f69df6a2e746e50a9ed4abedcc414974
unknown <k-b-v@yandex.ru>
2022-07-05 17:10:20 +0300

    New

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 80000;
+        int millya = 20;
+
+        int bonus = price / millya;
+
+        System.out.println("Начисленные милли: " + bonus);
+
+    }
+}
+
